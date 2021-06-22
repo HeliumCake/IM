aba9db77aeceedebbccf2f130dbb60857e5b84c1
Fallen_Breath <yqx791125833@gmail.com>
2021-06-22 21:56:22 +0800

    chat相关的一些入参变量的注释

--- src/main/java/com/tsinghua/course/Biz/Controller/Params/ChatParams/In/InviteUserToChatInParams.java
+++ src/main/java/com/tsinghua/course/Biz/Controller/Params/ChatParams/In/InviteUserToChatInParams.java
@@ -5,6 +5,9 @@ import com.tsinghua.course.Biz.BizTypeEnum;
 
 @BizType(BizTypeEnum.INVITE_USER_TO_CHAT)
 public class InviteUserToChatInParams extends BasicChatOperationInParams {
+	/**
+	 * 被邀请进入聊天的用户的用户名
+	 */
 	String invitedUser;
 
 	public String getInvitedUser() {
--- src/main/java/com/tsinghua/course/Biz/Controller/Params/ChatParams/In/ModifyGroupNameInParams.java
+++ src/main/java/com/tsinghua/course/Biz/Controller/Params/ChatParams/In/ModifyGroupNameInParams.java
@@ -5,6 +5,9 @@ import com.tsinghua.course.Biz.BizTypeEnum;
 
 @BizType(BizTypeEnum.MODIFY_CHAT_NAME)
 public class ModifyGroupNameInParams extends BasicChatOperationInParams {
+	/**
+	 * 聊天的新名字
+	 */
 	String newName;
 
 	public String getNewName() {
